9977f8c4158e9ea4cd6b85decfc2660a17db837a
whryuuki <929699294@qq.com>
2019-03-06 15:05:06 +0800

    no message

---
+++ important/电脑改造.js
@@ -0,0 +1,32 @@
+改造前，
+确认自己的u盘资料最新,非最新，
+更新[注意虚拟机和镜像文件，拿下来，centos和ubuntu]
+【maven库要最新】
+
+
+主要BASE软件
+apache发布工具
+Beyond Compare比较工具
+Google Chrome 谷歌浏览器+postMan+Postman Interceptor
+jmeter压测工具
+lanter蓝灯翻墙工具
+Navicat Premium;mysql管理工具
+RedisDesktopManager；redis管理工具
+git+SourceTree
+VMware Workstation Pro虚拟机，【一定要留镜像文件】
+Xshell 5+Xftp 5
+微信web开发者工具
+
+eclipse
+apache-maven-3.3.3  【maven库要最新】
+apache-tomcat-7.0.75
+jdk1.8
+LaoMaoTao_v9.2_1803
+mysql-5.6.17-winx64
+Ngrok
+Notepad++
+
+
+非开发软件
+百度网盘+福昕阅读器
+360+TeamViewer+UltraISO【抓包工具】+wps+迅雷+微信+qq
