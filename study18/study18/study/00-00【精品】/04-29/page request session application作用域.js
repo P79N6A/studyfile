c884eb6ffb18d837b1be22c05d4aac59e26d00b8
whryuuki <929699294@qq.com>
2019-03-06 13:58:32 +0800

    修改

--- study18/study18/study/00-00【精品】/04-29/page request session application作用域.js
+++
@@ -1,9 +0,0 @@
-几乎所有的Web开发语言都支持Session功能，Servlet也不例外。 
-Servlet/JSP中的Session功能是通过作用域(scope)这个概念来实现的。
-
-作用域分为四种，分别为：
-
-'page'	在'当前页面'有效(仅用于JSP中)
-'request'	在'当前请求'中有效
-'session'	在'当前会话'中有效
-'application'	在'所有应用程序'中有效
